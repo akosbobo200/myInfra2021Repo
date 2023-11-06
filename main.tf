9c697d2b4ad210f1cc0d5b85202f8c15e70cbea3
akosbobo200 <142356547+akosbobo200@users.noreply.github.com>
2023-11-06 12:31:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 
 
 #Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
+resource "aws_security_group" "jenkins-sg-2024" {
   name        = var.security_group
   description = "security group for jenkins"
                                         
