176a8392236b7385adc14f00dc371340c95dcb7e
akosbobo200 <142356547+akosbobo200@users.noreply.github.com>
2023-11-06 12:30:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region"{
+variable "aws_region" {
        description = "The AWS region to create things in." 
        default     = "us-east-2" 
 }
@@ -12,9 +12,10 @@ variable "instance_type" {
     description = "instance type for ec2" 
     default     =  "t2.micro" 
 }
+
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "my-jenkins-security-group-sep-2022" 
+    default     = "jenkins-sgroup-dec-2024" 
 }
 
 variable "tag_name" { 
@@ -25,4 +26,26 @@ variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance" 
     default     = "ami-024e6efaf93d85776" 
 }
-
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
